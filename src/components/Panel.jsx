898416eb9d174b80ef4ffe40aa9cd35645b00106
Nimira43 <n.rabone72@aol.com>
2024-12-26 00:03:12 +0000

    Brought in custom Panel code

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,9 +1,16 @@
-function Panel() {
-  
+import classNames from 'classnames'
+
+function Panel({ children, className, ...rest }) {
+  const finalClassNames = classNames(
+    'border rounded p-3 shadow bg-white w-full',
+    className
+  )
+
   return (
-    <div>
-      <h1>Panel</h1>
+    <div {...rest} className={finalClassNames}>
+      {children}
     </div>
   )
 }
+
 export default Panel
